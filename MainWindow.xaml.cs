49fcb25120a3a061a3975cfd8289cfe92c324097
Andrey616 <58079057+Andrey616@users.noreply.github.com>
2025-11-26 21:24:39 +0300

    Appload Database
    The data base has been changed to a different format.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YourProject.Services;
+
+namespace ESP_Keyboard
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			LoadMacros();
+			
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private ComboBox[] comboBoxes;
+
+		private void LoadMacros()
+		{
+
+			var dbService = new DatabaseService();
+			dbService.InitializeDatabase(); // Создаст БД при первом запуске
+			comboBoxes = new[] { ComboBoxBut1, ComboBoxBut2, ComboBoxBut3, ComboBoxBut4, ComboBoxBut5, ComboBoxBut6 };
+			foreach (var comboBox in comboBoxes)
+			{
+				comboBox.ItemsSource = dbService.GetMacros();
+				comboBox.DisplayMemberPath = "KeyCombination";
+				comboBox.SelectedValuePath = "Id";
+			}
+
+
+
+			
+		}
+	}
+}
